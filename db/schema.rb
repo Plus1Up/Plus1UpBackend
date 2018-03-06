555da564855a6823d988ce9498e9d5968ab74529
wzieba <wojciech.zieba@protonmail.com>
2018-03-06 21:20:29 +0100

    Move from SQLite to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180306164422) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "coaches", force: :cascade do |t|
     t.string "mailAddress"
     t.string "password"
