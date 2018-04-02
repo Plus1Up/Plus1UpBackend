a137c7e0eb7860d0cf0088e64e14961e9e21b683
wzieba <wojciech.zieba@protonmail.com>
2018-04-02 20:06:57 +0200

    Create basic docs for clients api

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'faker'
 gem 'simplecov', require: false, group: :test
 gem 'codacy-coverage', require: false, group: :test
 gem 'rack-cors', :require => 'rack/cors'
+gem 'apipie-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ config/initializers/apipie.rb
@@ -0,0 +1,9 @@
+Apipie.configure do |config|
+  config.app_name                = "Plus1UPBackend"
+  config.api_base_url            = "/api"
+  config.doc_base_url            = "/apidoc"
+  config.default_locale          = nil
+  config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
+  config.default_version         = "0.1"
+  config.app_info                = "Documentation for backend side of Plus 1UP project."
+end
